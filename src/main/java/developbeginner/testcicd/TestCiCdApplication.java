00b7158fce064e12b11b109ad2d965437f51aadf
developbeginner <148424130+developbeginner@users.noreply.github.com>
2023-10-19 23:48:29 +0900

    init: Springboot 프로젝트 생성

---
+++ src/main/java/developbeginner/testcicd/TestCiCdApplication.java
@@ -0,0 +1,13 @@
+package developbeginner.testcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCiCdApplication.class, args);
+    }
+
+}
